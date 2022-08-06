1e81b878aebf2ec826126b0afb1baa5ccda07b83
Andrej Dyck <43913051+andrej-dyck@users.noreply.github.com>
2022-08-06 13:21:07 +0200

    use stricter eslint config

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,12 +2,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  readonly name: string
 }
 
 export default function handler(
   _: NextApiRequest,
   res: NextApiResponse<Data>
-) {
+): void {
   res.status(200).json({ name: 'John Doe' })
 }
